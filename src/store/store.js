f02104f7e5d26e03239061a5c3f83839abf8ce91
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-03-30 15:19:44 +0200

    persistance

--- src/store/store.js
+++ src/store/store.js
@@ -1,8 +1,23 @@
 import { compose, createStore, applyMiddleware } from "redux";
-// import logger from "redux-logger";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
 
 import { rootReducer } from "./root-reducer";
-const middleWares = [];
-// const middleWares = [logger];
+const middleWares = [process.env.NODE_ENV === "development" && logger].filter(
+  Boolean
+);
+const persistConfig = {
+  key: "root",
+  storage: storage,
+  blacklist: ["user"],
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
 const composedEnchancer = compose(applyMiddleware(...middleWares));
-export const store = createStore(rootReducer, undefined, composedEnchancer);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnchancer
+);
+export const persistor = persistStore(store);
