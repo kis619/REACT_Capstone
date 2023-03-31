a602516b85944cd0ac5852b67986a818310c32a3
kristiyana milcheva <kristiyana.milcheva@gmail.com>
2023-03-31 12:48:12 +0200

    migrated cart

--- src/components/cart-dropdown/cart-dropdown.component.jsx
+++ src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -1,22 +1,25 @@
-import { useNavigate } from 'react-router-dom';
-import { useSelector } from 'react-redux';
+import { useNavigate } from "react-router-dom";
+import { useDispatch, useSelector } from "react-redux";
 
-import { selectCartItems } from '../../store/cart/cart.selector';
-import Button from '../button/button.component';
-import CartItem from '../cart-item/cart-item.component';
+import { selectCartItems } from "../../store/cart/cart.selector";
+import Button from "../button/button.component";
+import CartItem from "../cart-item/cart-item.component";
 
 import {
   CartDropdownContainer,
   EmptyMessage,
   CartItems,
-} from './cart-dropdown.styles';
+} from "./cart-dropdown.styles";
+import { setIsCartOpen } from "../../store/cart/cart.reducer";
 
 const CartDropdown = () => {
   const cartItems = useSelector(selectCartItems);
   const navigate = useNavigate();
+  const dispatch = useDispatch();
 
   const goToCheckoutHandler = () => {
-    navigate('/checkout');
+    dispatch(setIsCartOpen(false));
+    navigate("/checkout");
   };
 
   return (
